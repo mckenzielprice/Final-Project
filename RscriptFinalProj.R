58cd74f78059d068b5624e6a15e474befdfc3e53
McKenzie Price <mckenzie.price@emory.edu>
2024-08-26 15:10:38 -0400

    #3 create a figure = boxplot

--- RscriptFinalProj.R
+++ RscriptFinalProj.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 install.packages("schrute")
 library(schrute)
 library(tidyverse)
@@ -11,9 +10,6 @@ getwd()
 #Question 1: Create a {gtsummary} table of descriptive statistics about
 #your data (1 pt)
 
-=======
-#Question 1:
->>>>>>> 547be26ca619457a0a00015a89ac5960f20d5d42
 library(gtsummary)
 
 tbl_summary(
@@ -26,14 +22,13 @@ tbl_summary(
   	air_date ~ "Air Date"),
 
 	statistic = list(
-<<<<<<< HEAD
 		imdb_rating ~ "{mean} ({min}, {max})",
 		total_votes ~ "{sum}",
 		air_date ~ "({min}) to ({max})"),
 
 missing_text = "Missing"
 ) |>
-	modify_caption("**Summary of The Office Episodes by Season**")
+	modify_caption("**Summary of 'The Office' Episodes by Season**")
 
 #Question 2: Fit a regression and present well-formatted results from the
 #regression (1 pt); The regression doesn’t have to be of any particular
@@ -42,8 +37,6 @@ missing_text = "Missing"
 
 library(gtsummary)
 
-linear_model <- lm(imdb_rating ~ total_votes + air_date,
-									 data = office_data)
 poisson_model <- glm(total_votes ~ imdb_rating + season,
 										 family = poisson,
 										 data = office_data)
@@ -53,46 +46,18 @@ tbl_regression(
 	label = list(
 		imdb_rating ~ "IMDB Rating",
 		season ~ "Season"
-	))
+	)) |>
+	modify_caption("**Poisson Regression Analysis**")
 
-tbl_regression(
-	linear_model,
-	intercept = TRUE,
-	label = list(
-		total_votes ~ "Total Votes",
-		air_date ~ "Air Date"
-	))
+#Question 3: Create a figure (1 pt) It doesn’t need to look pretty; feel free
+#to adapt some of the examples from class, which were as simple as
+#hist(data$variable) and as complicated as the forest plot in the {broom}
+#section
 
-tbl_uvregression(
-	office_data,
-	y = season,
-	include = c(imdb_rating, total_votes, air_date),
-	method = lm)
+boxplot(imdb_rating ~ season, data = office_data,
+				main = "Box Plot Comparing 'The Office' IMDB Ratings by Season",
+				xlab = "Season",
+				ylab = "IMDB Rating",
+				outline = TRUE)
 
 
-
-
-
-
-=======
-		imdb_rating ~ "{mean}, {min}, {max}",
-		total_votes ~ "{max}",
-		air_date ~ "{min}, {max}"))
-
-missing_text = "Missing"
-add_p(test = list(all_continuous() ~ "t.test",
-									all_categorical() ~ "chisq.test")) |>
-	add_overall(col_label = "**Total**") |>
-	bold_labels() |>
-	modify_footnote(update = everything() ~ NA) |>
-	modify_header(label = "**Variable**", p.value = "**P**")
-
-)
-missing_text = "Missing"
-add_p(test = list(all_continuous() ~ "t.test",
-									all_categorical() ~ "chisq.test")) |>
-	add_overall(col_label = "**Total**") |>
-	bold_labels() |>
-	modify_footnote(update = everything() ~ NA) |>
-	modify_header(label = "**Variable**", p.value = "**P**")
->>>>>>> 547be26ca619457a0a00015a89ac5960f20d5d42
